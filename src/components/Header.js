0949a4aaf4fe47c8d5238ba98fb25b06bc1abf2a
rendy joshua <rendy.joshua@alpabit.com>
2022-02-18 19:23:31 +0700

    header restyling and update package.json

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,31 +4,21 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const StyledHeader = styled.div`
-  .bar {
-    z-index: 1;
-    position: fixed;
-    top: 0;
-    display: flex;
+  .barHeader {
+    position: floating;
     width: 100%;
+    display:flex;
+    justify-content: right;
     padding-left: 20px;
     padding-right: 20px;
-    height: 50px;
     align-items: center;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
-    background-color: #008080;
+    height: 70px;
+    box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.1);
+    background-color: darkBlue;
 
     a {
-      color: white;
-    }
-
-    .nav-items {
-      display: flex;
-      margin-left: auto;
-      .item {
-        margin-left: 20px;
-        margin-right: 20px;
-        cursor: pointer;
-      }
+      color: #dfe5f0;
+      text-decoration: underline overline;
     }
   }
 `;
@@ -36,24 +26,11 @@ const StyledHeader = styled.div`
 const Header = () => {
   return (
     <StyledHeader>
-      <div className="bar">
+      <div className="barHeader">
         <div className="logo">
           <Link to="/">
-            <h1>Dashboard</h1>
+            <h1>The Social Media</h1>
           </Link>
-        </div>
-        <div className="nav-items">
-          <div className="item">
-            <Link to="/about">About</Link>
-          </div>
-          <div className="item">
-            <a
-              href="https://github.com/arvindeva/react-dashboard"
-              target="blank"
-            >
-              <i className="github icon large" />
-            </a>
-          </div>
         </div>
       </div>
     </StyledHeader>
